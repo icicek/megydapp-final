836f408fbabbffd51d2085bc8753597c6a917fa0
Ilhan  Cicek <ilhancicek@Ilhan-MacBook-Pro.local>
2025-04-30 18:48:15 +0300

    mesaj

--- components/ClaimPanel.js
+++ components/ClaimPanel.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
+import Link from 'next/link';
 
 export default function ClaimPanel({
   walletAddress,
