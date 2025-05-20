853d3c91b3ced4579681b3f780cd67b148446012
Ilhan  Cicek <ilhancicek@Ilhan-MacBook-Pro.local>
2025-05-20 21:46:03 +0300

    “Cc”

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
-import HomePage from '@/components/HomePage';
+'use client';
+
+import Home from '@/components/HomePage';
 
 export default function Page() {
-  return <HomePage />;
+  return <Home />;
 }
